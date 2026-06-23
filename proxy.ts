176a09a8ff84de1f073e05d8af362ee332d08e24
amir-hosein-003 <ah.kanani003@gmail.com>
2026-06-23 16:54:34 +0330

    update next version

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 import { NextResponse } from "next/server";
 
-export default function middleware(req: any) {
+export default function proxy(req: any) {
   const { pathname } = req.nextUrl;
 
   if (pathname === "/") {
